a0199d9aedc7b96130da1b29966be10a45c06fa2
coohui <luizhuqi1543@163.com>
2018-04-22 17:32:41 +0800

    ajax封装完成

--- ajax/ajax.php
+++ ajax/ajax.php
@@ -1,31 +1,9 @@
 <?php 
+//post方式获取
 header('Content-type:application/json;charset=utf-8');  
 $raw = file_get_contents('php://input');
 $data = json_decode($raw,true);
 echo "你的名字：".$data['name']."，年龄：".$data['age']; 
-//echo $name;
-//echo "你的名字：".$data['name']."，年龄：".$data['age'];
-// if($age<18){
-// $sayList[]= array(	
-// 	'age'=>$age,
-// 	'name'=>$name,
-// 	'tips'=>'未成年'
-// );	
-// }else if($age>=18&&$age<40){
-// $sayList[]= array(	
-// 	'age'=>$age,
-// 	'name'=>$name,
-// 	'tips'=>'中年'
-// );
-// }else if($age>=40){
-// $sayList[]= array(	
-// 	'age'=>$age,
-// 	'name'=>$name,
-// 	'tips'=>'老年'
-// );
-// }
-
-// ini_set('display_errors', 1);
-// //echo json_encode($sayList);
-// echo 2342;
+//get方式获取
+//echo "你的名字：".$_GET['name']."，年龄：".$_GET['age']; 
 ?>
